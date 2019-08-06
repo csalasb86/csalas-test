eb9b8f4fd72b41af810465f335cf218ce01366bc
Carlos Salas <carlos.salas.bastias@gmail.com>
2019-08-06 17:36:42 -0400

    frontend v1

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,10 @@
 class VisitorsController < ApplicationController
+  def index
+    @routes = Route.order(:ends_at).all
+  end
+
+  def assign
+    SchedulerService.call
+    redirect_to root_path, notice: 'Rutas asignadas correctamente'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+  get 'visitors/assign', :to => 'visitors#assign', as: :visitors_assign
 end
